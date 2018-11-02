5aeef80c0ae0a2523adfe6e1599a7f7be230c099
Felipe Massicano <felipe@PATH-WP220LP11-M.local>
2018-11-02 16:06:53 -0500

    change zcat by gzcat to works in MacOs

--- scripts/average_gene_size.sh
+++ scripts/average_gene_size.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/bash
-
 NAME=$1
 
 # number of bases
-BP=`zcat $NAME | grep -v ">" | tr -d "\n" | wc -c`
+BP=`gzcat $NAME | grep -v ">" | tr -d "\n" | wc -c`
 
 # number of genes
-GENE_NUMB=`zcat $NAME | grep ">" | wc -l`
+GENE_NUMB=`gzcat $NAME | grep ">" | wc -l`
 
 echo "The average gene size"
+echo "BP:$BP"
+echo "GENE_NUMB:$GENE_NUMB"
 echo $BP / $GENE_NUMB | bc
