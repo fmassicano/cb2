2fd48d0e4443118703702090298b54ce56b94775
Felipe Massicano <massicano@gmail.com>
2018-11-07 22:31:46 -0600

    update the script

--- scripts/find_equal_ids.R
+++ scripts/find_equal_ids.R
@@ -2,8 +2,8 @@
 
 args <- commandArgs(trailingOnly = TRUE)
 
-file_one <- "data/ids.txt" #args[1]
-file_two <- "uniprot_ids" #args[2]
+file_one <- args[1] # ids that you are looking for
+file_two <- args[2] # alls possible ids
 
 df1 <- read.table(file = file_one,header = F)
 df2 <- read.table(file = file_two,header = F)
